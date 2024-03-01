a2ff33a68b0d19bc2995b59d3c06f5a524944e8a
Your Namexotrom <xotrom@gmail.com>
2024-03-02 02:34:30 +0300

    debug config

--- app/worker.ts
+++ app/worker.ts
@@ -38,14 +38,14 @@ const ollama = new ChatOllama({
 });
 
 const RESPONSE_SYSTEM_TEMPLATE = `You are an experienced researcher, expert at interpreting and answering questions based on provided sources. Using the provided context, answer the user's question to the best of your ability using the resources provided.
-Generate a concise answer for a given question based solely on the provided search results (URL and content). You must only use information from the provided search results. Use an unbiased and journalistic tone. Combine search results together into a coherent answer. Do not repeat text.
-If there is nothing in the context relevant to the question at hand, just say "Hmm, I'm not sure." Don't try to make up an answer.
+Generate a concise answer for a given question based solely on the provided search results (URL and content). Generate answer in russian. You must only use information from the provided search results. Use an unbiased and journalistic tone. Combine search results together into a coherent answer. Do not repeat text.
+If there is nothing in the context relevant to the question at hand, just say "Не уверен, что знаю ответ." Don't try to make up an answer.
 Anything between the following \`context\` html blocks is retrieved from a knowledge bank, not part of the conversation with the user.
 <context>
     {context}
 <context/>
 
-REMEMBER: If there is no relevant information within the context, just say "Hmm, I'm not sure." Don't try to make up an answer. Anything between the preceding 'context' html blocks is retrieved from a knowledge bank, not part of the conversation with the user.`;
+REMEMBER: If there is no relevant information within the context, just say "Не уверен, что знаю ответ." Don't try to make up an answer. Anything between the preceding 'context' html blocks is retrieved from a knowledge bank, not part of the conversation with the user.`;
 
 const responseChainPrompt = ChatPromptTemplate.fromMessages<{
   context: string;
--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   // (Optional) Export as a static site
   // See https://nextjs.org/docs/pages/building-your-application/deploying/static-exports#configuration
-  output: 'export', // Feel free to modify/remove this option
+  //output: 'export', // Feel free to modify/remove this option
 
   // Override the default webpack configuration
   webpack: (config, { isServer }) => {
